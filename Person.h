b203ab25dd869f98fd2ec84530bf0fdd5591824a
Blade Frisch <blade.frisch@gmail.com>
2016-10-10 21:42:29 -0400

    Added base class, initial version

---
+++ Person.h
@@ -0,0 +1,16 @@
+#ifndef
+#define
+
+#include <string>
+
+class Person {
+    private:
+        std::string name;
+        int         age;
+
+    public:
+        std::string getName()              { return name; }
+        void        setName(std::string n) { name = n; }
+        int         getAge()               { return age; }
+        void        setAge(int a)          { age = a; }
+};
