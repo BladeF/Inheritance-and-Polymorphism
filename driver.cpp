8f26319cf20893f2df7ab463345130d4c0f724af
Blade Frisch <blade.frisch@gmail.com>
2016-10-16 14:26:13 -0400

    Updated driver to use displayInfo functions

--- driver.cpp
+++ driver.cpp
@@ -9,9 +9,8 @@ int main(int argc, char **argv) {
     Person x;
     Student y("Blade", 23, 1234, 3.9);
 
-    cout << "Person: "  << x.getName() << " " << x.getAge() << endl;
-    cout << "Student: " << y.getName() << " " << y.getAge() << " "
-                        << y.getID()   << " " << y.getGPA() << endl;
+    x.displayInfo();
+    y.displayInfo();
 
     return EXIT_SUCCESS;
 }
