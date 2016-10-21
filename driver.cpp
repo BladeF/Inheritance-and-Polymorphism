6b690434e50151b669969dd98c133afef6e5347a
Blade Frisch <blade.frisch@gmail.com>
2016-10-21 16:01:15 -0400

    Changed driver to include an external function to illustrate binding

--- driver.cpp
+++ driver.cpp
@@ -5,12 +5,24 @@
 #include "Student.h"
 using namespace std;
 
+//Prototypes
+void displayWrapper(Person &p);
+
 int main(int argc, char **argv) {
     Person x;
     Student y("Blade", 23, 1234, 3.9);
 
-    x.displayInfo();
-    y.displayInfo();
+    displayWrapper(x);
+    displayWrapper(y);
 
     return EXIT_SUCCESS;
+}
+
+void displayWrapper(Person &p) {
+
+    cout << "Displaying information:\n\n";
+    p.displayInfo();
+    cout << endl << endl;
+
+    return;
 }
